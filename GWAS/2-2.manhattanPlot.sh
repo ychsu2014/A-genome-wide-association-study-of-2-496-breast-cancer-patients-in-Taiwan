0ec88bfbd76217acc79efe9130285c9a7661c6ab
Yu-Ching <ychsu20130517@gmail.com>
2022-06-21 12:23:25 +0800

    Delete the GWAS directory
    delete this directory for uploading codes of new version

--- GWAS/2-2.manhattanPlot.sh
+++
@@ -1,3 +0,0 @@
-python3 2_sub.manhattanPlot.py /home/iHiO10009/yuching/data/1.QC_GWAS_v2/impute2_keepFinalBC65yoControls_updatedSex_logistic_assoc_v2.assoc.logistic Breast_cancers /home/iHiO10009/yuching/data/1.QC_GWAS_v2/breastCancer_65yoControls_manPlot.png
-
-python3 2_sub.manhattanPlot.py /home/iHiO10009/yuching/data/1.QC_GWAS_v2/impute2_keepFinalBCControls_updatedSex_logistic_assoc_v2.assoc.logistic Breast_cancers /home/iHiO10009/yuching/data/1.QC_GWAS_v2/breastCancer_matchAgeControls_manPlot.png
